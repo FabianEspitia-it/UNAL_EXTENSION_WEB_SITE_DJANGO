62f2bc2b5959b45fc8938b7a511b5acbc1d65246
Fabián Espitia <fespitia@unal.edu.co>
2023-06-09 18:49:57 -0500

    Parte final del proyecto

--- extension/adminPanel/urls.py
+++ extension/adminPanel/urls.py
@@ -1,14 +1,19 @@
 from django.urls import path
-from .views import panelView, studentsListView, teacherListView, student_update, add_courses, update_courses
+from .views import *
 
 
 
 urlpatterns = [
     path("admin/", panelView, name="admin"),
+    path("admin/courses", coursesView, name="courses" ),
     path("admin/students/", studentsListView, name="students"),
     path("admin/teachers/", teacherListView, name="teachers"),
     path("admin/students/update_student", student_update, name="update_student" ),
     path("admin/courses/add_courses", add_courses, name="add_courses" ),
-    path("admin/courses/update_courses", update_courses, name="update_courses" )
-
+    path("admin/courses/update_courses", update_courses, name="update_courses" ),
+    path("admin/courses/delete_courses", deleteCourse, name="delete_courses"),
+    path("admin/services", servicesListView, name="services"),
+    path("admin/deans", decanosListView, name="deans"),
+    path("admin/projects", projectsListView, name="projects"),
+    path("inprogres/", inProgressView, name="inprogres")
 ]
--- extension/adminPanel/views.py
+++ extension/adminPanel/views.py
@@ -1,12 +1,20 @@
-from django.shortcuts import render
-from login_register.models import Estudiante, Profesor
+from django.shortcuts import render, redirect
+from login_register.models import Estudiante, Profesor, CursosDiplomados, Modalidad, ServicioDetalleEstado, Decano, Proyecto
 
 
 # Create your views here.
 
+def inProgressView(request):
+    return render(request, 'inprogres.html')
 
 def panelView(request):
     return render(request, 'panel.html')
+
+def coursesView(request):
+    cursos = CursosDiplomados.objects.all()
+    return render(request, 'cursos.html', {
+        "cursos": cursos
+    })
 
 def studentsListView(request):
     estudiantes = Estudiante.objects.all()
@@ -20,11 +28,85 @@ def teacherListView(request):
         'profesores': profesores
     })
 
-def student_update(request):
-    return render(request, 'update_student.html')
+def servicesListView(request):
+     servicios = ServicioDetalleEstado.objects.all()
+     return render(request, 'servicios.html', {
+          'servicios' : servicios
+     })
 
-def add_courses(request):
-    return render(request, 'add_courses.html')
+def decanosListView(request):
+     decanos = Decano.objects.all()
+     return render(request, 'decano.html', {
+          'decanos': decanos
+     }) 
+
+def projectsListView(request):
+    proyectos = Proyecto.objects.all()
+    return render(request, 'proyecto.html', {
+         'proyectos': proyectos
+    })
+
+
+def student_update(request):
+    try:
+        if request.method == 'POST':
+            estudiante = Estudiante.objects.get(id=request.POST["id"])
+            estudiante.nombre = request.POST["name"]
+            estudiante.correo = request.POST["mail"]
+            estudiante.rol = request.POST["role"]
+            estudiante.save()
+            return redirect("students")
+        else:
+            return render(request, 'update_student.html')
+    except:
+         error = "ID DONT MATCH"
+         return render(request, 'update_student.html', {
+              'error': error
+         })
+        
 
 def update_courses(request):
-    return render(request, 'update_courses.html')
+    try:
+        if request.method == 'POST':
+            curso = CursosDiplomados.objects.get(id=request.POST["id"])
+            curso.nombre = request.POST["nombre"]
+            curso.id_profesor = Profesor.objects.get(id = request.POST["profesor"])
+            curso.fecha = request.POST["fecha"]
+            curso.id_modalidad = Modalidad.objects.get(id = request.POST["modalidad"])
+            curso.save()
+            return redirect("courses")
+        else:
+            return render(request, 'update_courses.html')
+    except:
+         error = "COURSE DONT MATCH"
+         return render(request, 'update_courses.html', {
+              'error': error
+         })
+         
+
+def add_courses(request):
+    if request.method == 'POST':
+            CursosDiplomados.objects.create(nombre = request.POST["nombre"], 
+            id_profesor = Profesor.objects.get(id = request.POST["profesor"]), 
+            fecha = request.POST["fecha"], 
+            id_modalidad = Modalidad.objects.get(id = request.POST["modalidad"]))
+
+            return redirect("courses")
+
+    else:
+        return render(request, 'add_courses.html')
+    
+def deleteCourse(request):
+    if request.method == "POST":
+        try:
+                curso = CursosDiplomados.objects.get(nombre = request.POST["nombre"])
+                curso.delete()
+                return redirect("courses")
+        except:
+            error = "Course does not exits"
+            return render(request, "deleteCourse.html", {'error': error})
+    else:
+            return render(request, "deleteCourse.html")
+
+
+    
